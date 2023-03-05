89c702d9425b29eb2c63437b88ab6480f46e5943
Timur <51020673+Timau32@users.noreply.github.com>
2023-03-05 20:38:03 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const getData = async() => {
     const te = await fetch ('https://jsonplaceholder.typicode.com/posts')
     const fr = await te.json()
 
-    fr.map ((el) => {
-        re.innerHTML += `<div class = "di"> <p> userId: </p> ${el.userId} <p> id: </p> ${el.id} ${el.title} ${el.body} </div>`
+    const changedUsers = fr.map ((el) => {
+         return `<div class = "di"> <p> userId: </p> ${el.userId} <p> id: </p> ${el.id} ${el.title} ${el.body} </div>`
     })
+    re.innerHTML = changedUsers.join("");
 
 }
 getData()
